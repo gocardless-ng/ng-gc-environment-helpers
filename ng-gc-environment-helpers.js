aec907e72edadea4acdc74d266b4f521e6bded32
Philip Harrison <philip@mailharrison.com>
2013-10-22 11:48:29 +0100

    Initial

---
+++ ng-gc-environment-helpers.js
@@ -0,0 +1,26 @@
+(function(){ 'use strict';
+'use strict';
+
+angular.module('gc.environmentHelpers', [
+  'ngGcAppConfigService',
+]).factory('EnvironmentHelpers', [
+  'AppConfigService',
+  function EnvironmentHelpers(AppConfigService) {
+
+    var config = AppConfigService;
+
+    return {
+      isLiveProduction: function isLiveProduction() {
+        return config && config.current &&
+          config.current.liveProductionHost === config.current.host;
+      },
+      isSandboxProduction: function isSandboxProduction() {
+        return config && config.current &&
+          config.current.sandboxProductionHost === config.current.host;
+      }
+    };
+
+  }
+]);
+
+})();
---
+++ readme.md
@@ -0,0 +1,7 @@
+# ng-gc-environment-helpers
+
+![](https://circleci.com/gh/gocardless-ng/ng-gc-environment-helpers.png?circle-token=:circle-token)
+
+## Copyright and license
+
+Copyright 2013 GoCardless, Inc under the MIT license.
